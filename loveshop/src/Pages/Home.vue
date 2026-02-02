7a3351075c483b444816bb12b727c2d43c75d7f4
Nikolai <kolya7467@mail.ru>
2026-02-02 16:28:43 +0600

    удалил не нужные файлы и перекинул home в pages

--- loveshop/src/Pages/Home.vue
+++ loveshop/src/Pages/Home.vue
@@ -3,13 +3,8 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const goToLogin = () => {
-  router.push('/login')
-}
-
-const goToRegister = () => {
-  router.push('/register')
-}
+const goToLogin = () => router.push('/login')
+const goToRegister = () => router.push('/register')
 </script>
 
 <template>
@@ -17,12 +12,14 @@ const goToRegister = () => {
     <div class="text-center flex flex-col gap-6 bg-white p-8 rounded-lg shadow-lg">
       <h1 class="text-4xl font-bold text-gray-800">LoveShop</h1>
       <p class="text-gray-600 text-lg">Добро пожаловать!</p>
+
       <button
         @click="goToLogin"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded transition"
       >
         Войти
       </button>
+
       <button
         @click="goToRegister"
         class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded transition"
