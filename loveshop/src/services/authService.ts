976ac1c3eca2d1809f3ee9d31aed0edb19b85b1e
Nikolai <kolya7467@mail.ru>
2026-02-02 11:48:36 +0600

    сервиса взода и регистрации настроил

--- loveshop/src/services/authService.ts
+++ loveshop/src/services/authService.ts
@@ -1,13 +1,19 @@
+import { accessToken } from "@/states/stateManager";
+import axios from "axios";
+
 const API_URL = import.meta.env.VITE_API_URL
 
 export const authService = {
   async login(email: string, password: string) {
-    const response = await fetch(`${API_URL}/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+    await axios.post(`${API_URL}/login?useCookies=false`, { email, password }, {headers: {'Access-Control-Allow-Origin': '*'}})
+    .then(response => {
+      accessToken.value = response.data.accessToken;
+      return response.data;
     })
-    return response.json()
+    .catch(error => {
+      console.error('Error during login request:', error);
+      throw error;
+    });
   },
 
   async register(email: string, password: string) {
@@ -19,3 +25,4 @@ export const authService = {
     return response.json()
   },
 }
+
